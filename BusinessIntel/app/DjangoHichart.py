b910f584277545bf8fc13ad90eafd93672660683
asanka94 <asanka.jsndr@gmail.com>
2017-11-15 02:00:47 +0530

    highchart created for usecase2 ++work

---
+++ BusinessIntel/app/DjangoHichart.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render,render_to_response
+from django.template import loader
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+
+
+import pandas_highcharts
+from pandas_highcharts.core import serialize
+import pandas as pd
+import numpy as np
+
+products_df = pd.read_csv("app/input/products.csv")
+order_products_prior_df = pd.read_csv("app/input/order_products__prior.csv")
+
+def hichartGenerate(request, format=None):
+    dataset = order_products_prior_df.iloc[:,1:3]
+    dataset['no_of_orders'] = dataset.groupby('product_id')['add_to_cart_order'].transform(sum)
+    dataset=dataset.drop_duplicates('product_id', keep='first', inplace=False)
+    dataset=dataset.drop(['add_to_cart_order'], axis=1)
+    dataset=dataset.sort_values(by='no_of_orders',ascending=False)
+    dataset=dataset.iloc[:20,:]
+
+
+    dataset.rename(columns={'product_id': 'product'}, inplace=True)
+
+    dataset=dataset.join(products_df['product_name'],on='product',rsuffix='_id').set_index('product')
+
+    chart = serialize(
+            df=dataset,
+            render_to='Leak Values',
+           
+            chart_type="stock", title="What type of products customer purchase most often?",
+            use_index=False,
+            output_type='json')
+
+    return render(request, "app/DjangoHichartTest.html", context={"chart": chart})
+
---
+++ BusinessIntel/app/DjangoRestApi.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render,render_to_response
+from django.template import loader
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class Chartdata(APIView):
+   
+    authentication_classes = []
+    permission_classes = []
+
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+color = sns.color_palette()
+
+
+order_products_train_df = pd.read_csv("app/input/order_products__train.csv")
+order_products_prior_df = pd.read_csv("app/input/order_products__prior.csv")
+orders_df = pd.read_csv("app/input/orders.csv")
+products_df = pd.read_csv("app/input/products.csv")
+aisles_df = pd.read_csv("app/input/aisles.csv")
+departments_df = pd.read_csv("app/input/departments.csv")
+
+
+
+def get(self, request, format=None):
+    dataset = order_products_prior_df.iloc[:,1:3]
+    dataset['no_of_orders'] = dataset.groupby('product_id')['add_to_cart_order'].transform(sum)
+    dataset=dataset.drop_duplicates('product_id', keep='first', inplace=False)
+    dataset=dataset.drop(['add_to_cart_order'], axis=1)
+    dataset=dataset.sort_values(by='no_of_orders',ascending=False)
+    dataset=dataset.iloc[:20,:]
+
+
+    dataset.rename(columns={'product_id': 'product'}, inplace=True)
+
+    dataset=dataset.join(products_df['product_name'],on='product',rsuffix='_id').set_index('product')
+
+    # Data to plot
+    labels = dataset.iloc[:5,1]
+    sizes = dataset.iloc[:5,0]
+
+    json_dataset=dataset.to_json()
+        
+
+            
+      
+    data={
+        "sales":10,"customers":'Asanka',
+    }
+
+    return Response(json_dataset)
