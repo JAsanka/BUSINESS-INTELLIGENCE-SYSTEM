c4034d4adeba54b3d770c82f059fc9369c3a6d63
Rorounifix <rorounifix@gmail.com>
2017-11-28 15:17:15 +0300

    update

--- BusinessIntel/app/Charts/specialOccations.py
+++ BusinessIntel/app/Charts/specialOccations.py
@@ -18,50 +18,43 @@ class specialOccationData(APIView):
 
     def get(self, request, format=None):
 
-
         file = "app/Charts/new_sets.csv"
         df = pd.read_csv(file, index_col=0)
                     
-                    
-            # -------------------------------------------------------------------------------
+        start_date = "2011-01-01" # YYYY-MM-DD
+        end_date = "2011-02-20" # YYYY-MM-DD
+        prod_prof = df
+        prod_prof["TotalAmount"] = prod_prof["Quantity"]*prod_prof["UnitPrice"]
+        prod_prof.head()
 
-            start_date = "2011-01-01" # YYYY-MM-DD
-            end_date = "2011-02-20" # YYYY-MM-DD
-
-            prod_prof = df
-            prod_prof["TotalAmount"] = prod_prof["Quantity"]*prod_prof["UnitPrice"]
-            prod_prof.head()
-
-            prod_prof["TotalAmount"].sum() # Anually Sale   
-            total_amount = prod_prof.groupby("Description")["TotalAmount","Quantity","InvoiceDate"].agg({"TotalAmount":["sum","count"],
+        prod_prof["TotalAmount"].sum() # Anually Sale   
+        total_amount = prod_prof.groupby("Description")["TotalAmount","Quantity","InvoiceDate"].agg({"TotalAmount":["sum","count"],
                                                                             "Quantity":["sum","count"],
                                                                             })
-            total_amount.head()
+        total_amount.head()
 
 
 
-            def monthly_sale(start_date, end_date):
-                ta = total_amount
-                mask = (ta.index > np.datetime64(start_date)) & (ta.index <= np.datetime64(end_date))
-                out=ta[mask]["TotalAmount"]["sum"]
+        def monthly_sale(start_date, end_date):
+            ta = total_amount
+            mask = (ta.index > np.datetime64(start_date)) & (ta.index <= np.datetime64(end_date))
+            out=ta[mask]["TotalAmount"]["sum"]
 
-                # plt.figure(figsize=(12,8))
-                # ta[mask]["TotalAmount"]["sum"].plot()
-                # plt.xlabel("Lenght of Days", fontsize=20)
-                # plt.ylabel("Total Sale", fontsize=20)
-                # plt.title("Summary for Selected Date", fontsize=30)
-                # plt.xticks(rotation="vertical")
-                # plt.show()
+            # plt.figure(figsize=(12,8))
+            # ta[mask]["TotalAmount"]["sum"].plot()
+            # plt.xlabel("Lenght of Days", fontsize=20)
+            # plt.ylabel("Total Sale", fontsize=20)
+            # plt.title("Summary for Selected Date", fontsize=30)
+            # plt.xticks(rotation="vertical")
+            # plt.show()
 
-                return out
+            return out
 
-            print=monthly_sale(start_date, end_date)
+        print=monthly_sale(start_date, end_date)
 
             # -------------------------------------------------------------------------------
 
-        data={
-                "Total_Sessions":Total_Sessions,"TotalProfit":TotalProfit,"TotalSales":TotalSales,
-            }
+        data={"Total_Sessions":Total_Sessions,"TotalProfit":TotalProfit,"TotalSales":TotalSales}
 
         return Response(data)
 
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+#Python 3.x
+#pip 9.0.1
+#
 
+Install the requirements.txt first
+
+pip install -r requirements.txt
+
+then run python 
+
+python3 BusinessInter/manage.py runserver
